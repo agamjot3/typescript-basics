f6db998df9e7d1c6dee9c0761223689d84d43b6d
deepika-viewlift <deepika@viewlift.com>
2021-03-01 01:35:58 +0530

    Second Commit

--- main.js
+++ main.js
@@ -10,3 +10,59 @@ var isBeginner = true;
 var total = 10;
 var name = 'Vishwas';
 var sentence = "My name is " + name;
+console.log(sentence);
+total = 25;
+console.log(total);
+var list1 = [1, 2, 3];
+var list2 = [5, 6, 7];
+console.log(list1);
+console.log(list2);
+var person1 = ['Chris', 22];
+console.log(person1);
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+;
+var c = Color.Green;
+console.log(c);
+var random = 10;
+random = 'hello';
+var random1 = 'hello';
+random1.toUpperCase();
+console.log(random1);
+var multiType;
+multiType = 20;
+multiType = true;
+function add(num1, num2) {
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+var sum1 = add(10, 23);
+console.log(sum);
+var sum2 = add(5);
+console.log(sum2);
+function fullName(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+var p = {
+    firstName: "Bruce",
+    lastName: "Wayne"
+};
+fullName(p);
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log("Good Morning " + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Vishwas');
+console.log(emp1.employeeName);
+emp1.greet();
--- main.ts
+++ main.ts
@@ -20,3 +20,92 @@ let total: number = 10;
 let name: string = 'Vishwas';
 
 let sentence: string = `My name is ${name}`;
+
+
+console.log(sentence);
+
+total = 25;
+
+console.log(total);
+
+let list1: number[] = [1,2,3];
+let list2: Array<number> = [5,6,7];
+
+console.log(list1);
+console.log(list2);
+
+
+let person1: [string, number] = ['Chris', 22];
+
+console.log(person1);
+
+
+enum Color {Red, Green, Blue};
+
+let c: Color = Color.Green;
+
+console.log(c);
+
+
+let random: any = 10;
+random = 'hello';
+
+
+let random1: unknown = 'hello';
+
+(random1 as string).toUpperCase();
+
+console.log(random1);
+
+
+let multiType: number | boolean;
+multiType = 20;
+multiType = true;
+
+
+function add(num1: number, num2?: number){
+    if(num2)
+      return num1 + num2;
+    else
+      return num1;
+}
+
+let sum1 = add(10,23);
+console.log(sum);
+
+let sum2 = add(5);
+console.log(sum2);
+
+
+interface Person{
+    firstName: string,
+    lastName: string;
+}
+function fullName(person: Person}){
+      console.log(`${person.firstName} ${person.lastName}`);
+}
+
+let p ={
+    firstName: "Bruce",
+    lastName: "Wayne"
+}
+
+fullName(p);
+
+class Employee {
+    employeeName:string;
+
+    constructor(name: string){
+        this.employeeName = name;
+    }
+
+    greet(){
+        console.log(`Good Morning ${this.employeeName}`);
+    }
+
+
+}
+
+let emp1 = new Employee('Vishwas');
+console.log(emp1.employeeName);
+emp1.greet();
